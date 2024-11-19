7b44e3ba5725154bc2d944e6f3102143f0436d6a
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-11-19 13:11:51 +0100

    fskgohehojd

---
+++ steg2/Program.cs
@@ -0,0 +1,52 @@
+﻿// 1
+List<string> toys = ["bil", "tåg", "playmobile", "lego", "transformers"];
+
+// 2
+for (int i = 0; i < toys.Count; i++)
+{
+    Console.WriteLine(toys[i]);
+}
+
+// 3
+List<string> comrades = ["Nilo", "Alexander1", "Alexander2", "Jack", "Teo"];
+
+// 4
+List<int> number123 = [9, 8, 3, 2, 6];
+
+// 5
+for (int i = 0; i < toys.Count; i++)
+{
+    Console.WriteLine($"{comrades[i]} ger {toys[i]} betyget {number123[i]}");
+}
+
+// 6
+List<string> cities = [];
+
+// 7 och 8
+string city = "";
+
+while(city != "hm")
+{
+    city = Console.ReadLine();
+    cities.Add(city);
+}
+for (int i = 0; i < cities.Count; i++)
+{
+    Console.WriteLine(cities[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
